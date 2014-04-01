840aaab5b8c6f7de5b98f737742b81df77cca53b
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-04-01 16:26:50 +0900

    change graph face

--- mkplot.sh
+++ mkplot.sh
@@ -5,14 +5,15 @@ sed "s/XXX/$RESDIR/g" <<'END'
 system("rm -f result.png")
 
 #set terminal pngcairo size 480,640
-set terminal pngcairo size 1024,768 font "Courier Bold,17"
+#set terminal pngcairo size 1024,768 font "Courier Bold,17"
+set terminal pngcairo size 1024,768 enhanced mono font "Luxi Mono,17"
 set output "result.png"
 
 set key left top
 set grid
 set log xy
 
-set format xy "10^%L"
+set format xy "10^{%L}"
 
 set xlabel "filesize [bytes]"
 set ylabel "end-start [sec]"
@@ -24,7 +25,7 @@ END
 
 for f in $RESDIR/result-buf-*.txt; do
 	BUFFERSIZE=$(echo $f | cut -d- -f3 | cut -d. -f1)
-	echo "	\"$f\" using 1:2 title \"buffering(buffersize=10^$(python2 -c "import math; print int(math.log10($BUFFERSIZE))"))\" with linespoints, \\"
+	echo "	\"$f\" using 1:2 title \"buffering(buffersize=10^{$(python2 -c "import math; print int(math.log10($BUFFERSIZE))")})\" with linespoints, \\"
 done
 
 sed "s/XXX/$RESDIR/g" <<'END'
